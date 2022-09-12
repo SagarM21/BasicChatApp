c6147349df0ddd021edcd39f27bddfb7a570943d
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-12 13:58:44 +0530

    react router dom setup, pages created, proxy added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import ChatPage from "./pages/ChatPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
-	return <div>Hello</div>;
+	return (
+		<div>
+			<Routes>
+				<Route path='/' element={<HomePage />} exact />
+				<Route path='/chats' element={<ChatPage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<ChakraProvider>
-			<App />
-		</ChakraProvider>
+		<Router>
+			<ChakraProvider>
+				<App />
+			</ChakraProvider>
+		</Router>
 	</React.StrictMode>
 );
