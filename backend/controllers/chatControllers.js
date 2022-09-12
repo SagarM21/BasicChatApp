bd64887ecdec59bd677d1be63934a251d896991f
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-12 21:25:43 +0530

    searching backend

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -66,4 +66,18 @@ const fetchChats = asyncHandler(async (req, res) => {
 	}
 });
 
-module.exports = { accessChat, fetchChats };
+const allUsers = asyncHandler(async (req, res) => {
+	const keyword = req.query.search
+		? {
+				$or: [
+					{ name: { $regex: req.query.search, $options: "i" } },
+					{ email: { $regex: req.query.search, $options: "i" } },
+				],
+		  }
+		: {};
+
+	const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+	res.send(users);
+});
+
+module.exports = { accessChat, fetchChats, allUsers };
