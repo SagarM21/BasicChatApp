80a841393df87b71c8af1a52abf69b3b387c1512
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-12 13:35:05 +0530

    node server with some api for testing fake data

---
+++ backend/data/data.js
@@ -0,0 +1,104 @@
+const chats = [
+	{
+		users: [
+			{
+				name: "John Doe",
+				email: "john@gmail.com",
+			},
+			{
+				name: "Sagar",
+				email: "sagar@gmail.com",
+			},
+		],
+		_id: "617a077e18c25468bc7c4dd4",
+		chatName: "John Doe",
+	},
+	{
+		users: [
+			{
+				name: "Guest User",
+				email: "guest@gmail.com",
+			},
+			{
+				name: "Sagar",
+				email: "sagar@gmail.com",
+			},
+		],
+		_id: "617a077e18c25468b27c4dd4",
+		chatName: "Guest User",
+	},
+	{
+		users: [
+			{
+				name: "Anthony",
+				email: "anthony@gmail.com",
+			},
+			{
+				name: "Sagar",
+				email: "sagar@gmail.com",
+			},
+		],
+		_id: "617a077e18c2d468bc7c4dd4",
+		chatName: "Anthony",
+	},
+	{
+		users: [
+			{
+				name: "John Doe",
+				email: "jon@gmail.com",
+			},
+			{
+				name: "Sagar",
+				email: "sagar@gmail.com",
+			},
+			{
+				name: "Guest User",
+				email: "guest@gmail.com",
+			},
+		],
+		_id: "617a518c4081150716472c78",
+		chatName: "Friends",
+		groupAdmin: {
+			name: "Guest User",
+			email: "guest@gmail.com",
+		},
+	},
+	{
+		users: [
+			{
+				name: "Jane Doe",
+				email: "jane@gmail.com",
+			},
+			{
+				name: "Sagar",
+				email: "sagar@gmail.com",
+			},
+		],
+		_id: "617a077e18c25468bc7cfdd4",
+		chatName: "Jane Doe",
+	},
+	{
+		users: [
+			{
+				name: "John Doe",
+				email: "jon@gmail.com",
+			},
+			{
+				name: "Sagar",
+				email: "sagar@gmail.com",
+			},
+			{
+				name: "Guest User",
+				email: "guest@gmail.com",
+			},
+		],
+		_id: "617a518c4081150016472c78",
+		chatName: "VITians",
+		groupAdmin: {
+			name: "Guest User",
+			email: "guest@gmail.com",
+		},
+	},
+];
+
+module.exports = chats;
