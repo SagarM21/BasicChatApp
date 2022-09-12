14afae8c912c3d4101f26de17f2046d375ee3fb1
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-12 16:17:18 +0530

    api for one on one chat created

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const chats = require("./data/data");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
@@ -18,16 +19,8 @@ app.get("/", (req, res) => {
 	res.send("API is running");
 });
 
-// app.get("/api/chat", (req, res) => {
-// 	res.send(chats);
-// });
-
-// app.get("/api/chat/:id", (req, res) => {
-// 	const singleChat = chats.find((chat) => chat._id !== req.params.id);
-// 	res.send(singleChat);
-// });
-
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 // for the errors
 app.use(notFound);
