ca662dd2be354ce58eb5c7ca95cb65f3ce64f422
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-09-13 12:08:07 +0530

    singleChat functioning - ui remains

--- frontend/src/components/ChatPages/SingleChat.js
+++ frontend/src/components/ChatPages/SingleChat.js
@@ -3,12 +3,91 @@ import ProfileModal from "./ProfileModal";
 import { FormControl } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
 import { Box, Text } from "@chakra-ui/layout";
-import { IconButton, Spinner } from "@chakra-ui/react";
+import { IconButton, Spinner, useToast } from "@chakra-ui/react";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { getSender, getSenderFull } from "../../config/ChatLogics";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+	const [messages, setMessages] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [newMessage, setNewMessage] = useState();
 	const { user, selectedChat, setSelectedChat } = ChatState();
+
+	const toast = useToast();
+
+	const fetchMessages = async () => {
+		if (!selectedChat) return;
+
+		try {
+			const config = {
+				headers: {
+					Authorization: `Bearer ${user.token}`,
+				},
+			};
+
+			setLoading(true);
+
+			const { data } = await axios.get(
+				`/api/message/${selectedChat._id}`,
+				config
+			);
+			setMessages(data);
+			setLoading(false);
+		} catch (error) {
+			toast({
+				title: "Error Occurred!",
+				description: "Failed to Load the Messages",
+				status: "error",
+				duration: 5000,
+				isClosable: true,
+				position: "bottom",
+			});
+		}
+	};
+
+	useEffect(() => {
+		fetchMessages();
+	}, [selectedChat]);
+
+	const sendMessage = async (event) => {
+		if (event.key === "Enter" && newMessage) {
+			try {
+				const config = {
+					headers: {
+						"Content-type": "application/json",
+						Authorization: `Bearer ${user.token}`,
+					},
+				};
+				setNewMessage("");
+				const { data } = await axios.post(
+					"/api/message",
+					{
+						content: newMessage,
+						chatId: selectedChat,
+					},
+					config
+				);
+
+				console.log(data);
+				setMessages([...messages, data]);
+			} catch (error) {
+				toast({
+					title: "Error Occurred!",
+					description: "Failed to send the Message",
+					status: "error",
+					duration: 5000,
+					isClosable: true,
+					position: "bottom",
+				});
+			}
+		}
+	};
+
+	const typingHandler = (e) => {
+		setNewMessage(e.target.value);
+	};
 	return (
 		<>
 			{selectedChat ? (
@@ -46,7 +125,32 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 						borderRadius='lg'
 						overflowY='hidden'
 					>
-						{/* MESSAGES UI */}
+						{loading ? (
+							<Spinner
+								size='xl'
+								w={20}
+								h={20}
+								alignSelf='center'
+								margin='auto'
+							/>
+						) : (
+							<div>{/* messages */}</div>
+						)}
+
+						<FormControl
+							onKeyDown={sendMessage}
+							id='first-name'
+							isRequired
+							mt={3}
+						>
+							<Input
+								variant='filled'
+								bg='#E0E0E0'
+								placeholder='Enter a message..'
+								value={newMessage}
+								onChange={typingHandler}
+							/>
+						</FormControl>
 					</Box>
 				</>
 			) : (
